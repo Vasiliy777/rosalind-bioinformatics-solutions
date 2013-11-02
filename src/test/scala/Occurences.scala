7dcb2a3799a22be15d524c263c87d80f9278dc63
Tomasz Bartczak <tomasz.bartczak@pragmatists.pl>
2013-11-02 18:21:01 +0100

    stepic 'mismatched words' solved

--- src/test/scala/Occurences.scala
+++ src/test/scala/Occurences.scala
@@ -1,3 +1,5 @@
+import org.apache.commons.lang3.StringUtils
+
 class Occurences {
 
   def allOccurences(genome: String, pattern: String): Iterator[Int] = {
@@ -5,4 +7,15 @@ class Occurences {
       .collect {case a:(String,Int) => a._2}
   }
 
+  def allMismatchedOccurences(genome: String, pattern: String, mismatchCount: Int): Iterator[Int] = {
+    genome.sliding(pattern.length).zipWithIndex.filter(matchingPattern(pattern, mismatchCount))
+      .collect {case a:(String,Int) => a._2}
+  }
+
+
+  def matchingPattern(pattern: String, mismatchCount: Int): ((String, Int)) => Boolean = {
+    (p: Pair[String, Int]) => {
+      p._1.zip(pattern).count((p:Pair[Char,Char]) => p._1 != p._2) <= mismatchCount
+    }
+  }
 }
