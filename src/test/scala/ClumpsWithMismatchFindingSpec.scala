0e56394561293c62ca1b05a0490b7ed1670d1a01
Tomasz Bartczak <tomasz.bartczak@pragmatists.pl>
2013-11-05 21:36:17 +0100

    stepic 'kmer with mismatches' refactored just a bit

--- src/test/scala/ClumpsWithMismatchFindingSpec.scala
+++ src/test/scala/ClumpsWithMismatchFindingSpec.scala
@@ -41,16 +41,6 @@ class ClumpsWithMismatchFindingSpec extends FlatSpec with Matchers {
     assert(mostFrequentMismatchedKmers === Set("GCACACAGAC", "GCGCACACAC"))
   }
 
-  "test case 1" should "be found" in {
-    val genome = "CCCGTCACCCGTCCCCCTCATCATCTTCTCCCCGTCATTCTCCCTCACCCCCCCGCCTCCCCGTCCCTTCCCCCTCTCATCACCCCTCATCTCCCCGTCATCACCCGCCCGCCCCCGCCCGCCCGTTCCCCGTTCCCCCCGCCCCTCATCCCCGTTCTCATTCCCCGTCTTCCCCGCCCCCGTTCCCTCTCACCCGTCCCCGCCCGTCTTCTCTCATTCCCCGCCTCCCCCTCACCCGTTCCCTCACCCGCCCGTCTCTCTTCCCCGCCCCTCTCCCTTCCCCGTTCTCTCACCTCACCCGTCTTCTCTTCCCCGCCCGCC"
-    val kmerLength =  10
-    val mismatchCount = 3
-
-    val mostFrequentMismatchedKmers = mostFrequent(genome,kmerLength,mismatchCount)
-    println(mostFrequentMismatchedKmers.mkString(" "))
-    assert(mostFrequentMismatchedKmers !== Set("CCCCCCCCCC"))
-  }
-
   "test case 2" should "be found" in {
     val genome = "TGCATGCATGCGTCTGCAGGTAGGTAGGTAGTCTATGCAGTCGGTATGCAGTCTATGCGGTAGGTATGCATGCTATGCTAGTCTGCTGCATGCAGTCGGTATGCATGCGTCTAGTCGGTATGCGGTAGGTATGCTAGGTAGTCTGCGGTAGTCTGCAGTCTAGTCTGCTGCAGGTAGTCGTCGGTATGCAGTCTGCTGCTGCATGCAGTCTGCTGCATGCTGCGTCTGCTGCTGCATAGGTATATGCTATGCGTCGGTAGGTAGGTATGCATGCGTCGGTATGCTGCAGGTATATGCATGCTGCAGTCTGCGGTATGCGGTATGCA"
     val kmerLength =  9
@@ -70,41 +60,25 @@ class ClumpsWithMismatchFindingSpec extends FlatSpec with Matchers {
     assert(mostFrequentMismatchedKmers === Set("GG"))
   }
 
-  class Mismatched(val kmer:String) {
-    override def toString(): String = kmer
-
-    override def hashCode(): Int = kmer.hashCode
-
-    override def equals(obj: scala.Any): Boolean = obj match {
-      case obj:Mismatched => obj.kmer.equals(this.kmer)
-      case _ => false
-    }
-  }
-
   def mostFrequent(genome: String, kmerLength: Int, mismatchCount: Int) = {
-    val allPossibleMismatched: List[Mismatched] = genome.sliding(kmerLength).toList.flatMap(kmer => possibleMismatched(kmer, mismatchCount))
-    println(allPossibleMismatched)
-    val groupped: Map[Mismatched, List[Mismatched]] = allPossibleMismatched.groupBy(identity)
-    println(groupped)
-    val maxBy: Iterable[Mismatched] = allMaxBy(groupped,(p:Pair[Mismatched,List[Mismatched]]) => p._2.size)
-    println(maxBy)
-    val allMaxed: Map[Mismatched, List[Mismatched]] = groupped.filterKeys(maxBy.toList.contains(_))
-    println(allMaxed)
-    val result = allMaxed.collect { case entry:Pair[_,List[Mismatched]] => entry._2}.flatten
-    println(result)
-    result.collect { case m:Mismatched => m.kmer }.toSet
+    val allPossibleMismatched: List[String] = genome.sliding(kmerLength).toList.flatMap(kmer => possibleMismatched(kmer, mismatchCount))
+    val groupped: Map[String, List[String]] = allPossibleMismatched.groupBy(identity)
+    val maxBy: Iterable[String] = allMaxBy(groupped,(p:Pair[String,List[String]]) => p._2.size)
+    val allMaxed: Map[String, List[String]] = groupped.filterKeys(maxBy.toList.contains(_))
+    val result = allMaxed.collect { case entry:Pair[_,List[String]] => entry._2}.flatten
+    result.toSet
   }
 
   def substitute(string: String, index: Int, toSubstitute: Char): String = string.substring(0,index) + toSubstitute + string.substring(index+1)
 
-  def possibleMismatchedRec(mismatched: List[Mismatched], mismatchCount: Int): List[Mismatched] = {
+  def possibleMismatchedRec(mismatched: List[String], mismatchCount: Int): List[String] = {
     if(mismatchCount == 0) {
       mismatched
-    } else for (kmer:Mismatched <- mismatched; p <- kmer.kmer.zipWithIndex; nuc <- List('A','T','C','G');a <- possibleMismatchedRec(List(new Mismatched(substitute(kmer.kmer,p._2,nuc))),mismatchCount-1)) yield a
+    } else for (kmer:String <- mismatched; p <- kmer.zipWithIndex; nuc <- List('A','T','C','G');a <- possibleMismatchedRec(List(substitute(kmer,p._2,nuc)),mismatchCount-1)) yield a
   }
 
-  def possibleMismatched(kmer: String, mismatchCount: Int): GenTraversableOnce[Mismatched] = {
-    possibleMismatchedRec(List(new Mismatched(kmer)),mismatchCount).toSet
+  def possibleMismatched(kmer: String, mismatchCount: Int): GenTraversableOnce[String] = {
+    possibleMismatchedRec(List(kmer),mismatchCount).toSet
   }
 
   def allMaxBy[KEY,VALUE](someMap: Map[KEY, VALUE], function: ((KEY, VALUE)) => Int): Iterable[KEY] = {
